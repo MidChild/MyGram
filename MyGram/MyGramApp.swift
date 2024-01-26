a934de8715114203f2d7e57ba810f279d529af39
Aimé Banda <aime.banda@diiage.org>
2024-01-26 16:37:20 +0100

    Initial Commit

---
+++ MyGram/MyGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGramApp.swift
+//  MyGram
+//
+//  Created by Aimé Banda on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
